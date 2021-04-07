dd62d3b67a86ae5889e276f795f016b874558083
Md Sirajul Islam Sojib <sirajulislam1123@gmail.com>
2021-04-07 12:18:37 +0600

    MockTodoData added for mocking the api

---
+++ TodoData/MockTodoData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoManagerApi.TodoData
+{
+    public class MockTodoData
+    {
+    }
+}
