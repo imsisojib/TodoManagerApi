df2df4c13e738739dac30ec147e31a4f917a1f64
imsisojib <sirajulislam1123@gmail.com>
2021-10-07 18:29:39 +0600

    seed data into table or demo data into databse

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -12,6 +12,24 @@ namespace TodoManagerApi.Models
         { }
 
         public DbSet<Todo>  Todos { get; set; }
+
+
+        //initially seed data into table
+        //each enitity can be added in this below method.
+        //after updating: add-migration, updated-database then done.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            builder.Entity<Todo>().HasData(
+                    new Todo { 
+                        Id = new Guid(),
+                        Tittle = "Demo Tittle",
+                        Description = "Demo Description Here"
+                    }
+                ) ;
+        }
+
     }
 
 }
